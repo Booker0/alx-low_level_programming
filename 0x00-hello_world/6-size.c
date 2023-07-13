9a5959f98df739538158aa492477dab96025e586
Booker0 <yungg20@gmail.com>
2023-07-13 12:02:42 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,7 +4,7 @@
  *
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
 	printf("Size of a char: %1u byte(s)\n", sizeof(char));
 	printf("Size of a int: %1u byte(s)\n", sizeof(int));
